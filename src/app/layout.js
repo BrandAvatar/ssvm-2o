a57779da7cb75d6ac772f50e857ac9b47286cd88
xtracut-Arun <arun@xtracut.com>
2025-05-26 14:53:26 +0530

    style error fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -44,7 +44,7 @@ export default function RootLayout({ children }) {
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M79FS9NN" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M79FS9NN" height="0" width="0" style={{display:'none', visibility:'hidden'}}></iframe></noscript>
 
 
         <AnimationProvider>{children}</AnimationProvider>
