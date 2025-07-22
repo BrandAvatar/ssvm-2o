54354c2281d68af51d3d9003ae9aacfbeafebd8a
amulxc <amul@xtracut.com>
2025-07-22 16:35:34 +0530

    Update service images and titles in services-items.js; adjust styles in _service.scss for better layout and responsiveness

--- src/constant/photo-studio/services-items.js
+++ src/constant/photo-studio/services-items.js
@@ -1,37 +1,44 @@
 const SERVISES = [
     {
       id: 1,
-      title: "Updating ...",
+      title: "Attakalari",
       link: "/#",
-      image: "https://ssvmtransformationindia.s3.ap-south-1.amazonaws.com/images/soon.png",
+      image: "https://ssvmtransformationindia.s3.ap-south-1.amazonaws.com/images/workshop-01.webp",
       alt: "service-thumb"
     },
     {
       id: 2,
-      title: "Updating ...",
+      title: "Vilas Nayak",
       link: "/#",
-      image: "https://ssvmtransformationindia.s3.ap-south-1.amazonaws.com/images/soon.png",
+      image: "https://ssvmtransformationindia.s3.ap-south-1.amazonaws.com/images/workshop-02.webp",
       alt: "service-thumb"
     },
     {
       id: 3,
-      title: "Updating ...",
+      title: "No Treble",
       link: "/#",
-      image: "https://ssvmtransformationindia.s3.ap-south-1.amazonaws.com/images/soon.png",
+      image: "https://ssvmtransformationindia.s3.ap-south-1.amazonaws.com/images/workshop-03.webp",
       alt: "service-thumb"
     },
     {
       id: 4,
-      title: "Updating ...",
+      title: "SSVM X Vaja & Antony",
       link: "/#",
-      image: "https://ssvmtransformationindia.s3.ap-south-1.amazonaws.com/images/soon.png",
+      image: "https://ssvmtransformationindia.s3.ap-south-1.amazonaws.com/images/workshop-04.webp",
       alt: "service-thumb"
     },
     {
       id: 5,
-      title: "Updating ...",
+      title: "SSVM X Attakalari",
       link: "/#",
-      image: "https://ssvmtransformationindia.s3.ap-south-1.amazonaws.com/images/soon.png",
+      image: "https://ssvmtransformationindia.s3.ap-south-1.amazonaws.com/images/workshop-05.webp",
+      alt: "service-thumb"
+    },
+    {
+      id: 6,
+      title: "Amrita Binito Chetry",
+      link: "/#",
+      image: "https://ssvmtransformationindia.s3.ap-south-1.amazonaws.com/images/workshop-6.webp",
       alt: "service-thumb"
     }
   ];
