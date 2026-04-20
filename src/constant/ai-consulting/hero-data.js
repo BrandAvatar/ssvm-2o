3534c38c48da0c5e96ce3f5a53e6c0c2ea641ef1
BrandAvatar <accounts@brandavatar.in>
2026-04-20 15:42:11 +0530

    Delete src directory

--- src/constant/ai-consulting/hero-data.js
+++
@@ -1,9 +0,0 @@
-const heroData = {
-
-    SplineLink:'https://prod.spline.design/fP0LH65i8bXQDQjZ/scene.splinecode',
-    heroInfo:'Nor again is there anyone who loves or pursues or desires to obtain pains',
-    heroTitle: 'ai Consulting',
-    btnLink: '/contact',
-    btnText: 'Get Started Today',
-}
-export default heroData;
